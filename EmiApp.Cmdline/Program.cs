86536f731fc489e42c14282d8c0bcb190f6576d3
varshjit <89901335+varshjit-gawas-kilowott@users.noreply.github.com>
2021-12-08 13:49:23 +0530

    Serverside

---
+++ EmiApp.Cmdline/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EmiApp.Cmdline
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var emiRequest = new Request() { Principal = 100000, InterestRateInPercentage = 12, LoanDurationInYearCount = 2 };
+            ICalculator emiCalculator = new Calculator();
+
+            #region
+            var emiMonthly = emiCalculator.CalculateMonthlyEmi(emiRequest);
+
+            if (string.IsNullOrEmpty(emiMonthly.ErrorMessage))
+                Console.WriteLine($"Monthly EMI is {emiMonthly.EmiPayment}");
+            else
+                Console.WriteLine(emiMonthly.ErrorMessage);
+            #endregion
+
+
+            // fill other regions below...
+            #region
+            var emiDaily = emiCalculator.CalculateDailyEmi(emiRequest);
+
+            if (string.IsNullOrEmpty(emiMonthly.ErrorMessage))
+                Console.WriteLine($"Daily EMI is {emiDaily.EmiPayment}");
+            else
+                Console.WriteLine(emiMonthly.ErrorMessage);
+            #endregion
+
+            #region
+            var emiContinous = emiCalculator.CalculateContinousEmi(emiRequest);
+
+            if (string.IsNullOrEmpty(emiMonthly.ErrorMessage))
+                Console.WriteLine($"Continous EMI is {emiContinous.EmiPayment}");
+            else
+                Console.WriteLine(emiMonthly.ErrorMessage);
+            #endregion
+        }
+    }
+}
---
+++ EmiAppAPI/Controllers/EmiController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EmiApp;
+
+namespace EmiAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmiController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/[controller]/CalculateMonthlyEmi")]
+        public Response CalculateMonthlyEmi(Request request)
+        {
+            ICalculator emiCalculator = new Calculator();
+
+            var response = new Response();
+
+            var emiMonthly = emiCalculator.CalculateMonthlyEmi(request);
+
+            return emiMonthly;
+        }
+        [HttpPost]
+        [Route("api/[controller]/CalculateDailyEmi")]
+        public Response CalculateDailyEmi(Request request)
+        {
+            ICalculator emiCalculator = new Calculator();
+
+            var response = new Response();
+
+            var emiDaily = emiCalculator.CalculateDailyEmi(request);
+
+            return emiDaily;
+        }
+        [HttpPost]
+        [Route("api/[controller]/CalculateContinousEmi")]
+        public Response CalculateContinousEmi(Request request)
+        {
+            ICalculator emiCalculator = new Calculator();
+
+            var response = new Response();
+
+            var emiContinous = emiCalculator.CalculateContinousEmi(request);
+
+            return emiContinous;
+        }
+    }
+}
